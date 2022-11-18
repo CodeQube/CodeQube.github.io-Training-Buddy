a1cabff566fddf754cd19892d6d205a156154399
xNemix <46863616+xNemix@users.noreply.github.com>
2022-11-18 13:45:57 +0100

    Update globalController.js

--- controller/globalController.js
+++ controller/globalController.js
@@ -1,48 +1,57 @@
 function setActivePage(page) {
-	switch (page) {
-		case "home":
-			model.app.activePage = "home";
-			mainView();
-			break;
-		case "calendar":
-			model.app.activePage = "calendar";
-			model.app.subPage = "upcoming";
-			mainView();
-			break;
-		case "createNew":
-			model.app.activePage = "createNewEvent";
-			mainView();
-			break;
-		case "leaderboard":
-			model.app.activePage = "leaderboard";
-			mainView();
-			break;
-		case "profile":
-			model.app.activePage = "profile";
-			mainView();
-			break;
-	}
+  switch (page) {
+    case 'home':
+      model.app.activePage = 'home';
+      model.app.state.selectedCard = null;
+      mainView();
+      break;
+    case 'calendar':
+      model.app.activePage = 'calendar';
+      model.app.subPage = 'upcoming';
+      model.app.state.selectedCard = null;
+
+      mainView();
+      break;
+    case 'createNew':
+      model.app.activePage = 'createNewEvent';
+      model.app.state.selectedCard = null;
+
+      mainView();
+      break;
+    case 'leaderboard':
+      model.app.activePage = 'leaderboard';
+      model.app.state.selectedCard = null;
+
+      mainView();
+      break;
+    case 'profile':
+      model.app.activePage = 'profile';
+      model.app.state.selectedCard = null;
+
+      mainView();
+      break;
+  }
 }
 
 function attendEvent(eventId) {
-	let people = [];
-	const events = model.data.eventParticipants.filter(
-		(event) => event.eventId === eventId
-	);
-	events.forEach((person) => people.push(person.userName));
-	if (
-		people.includes(model.app.state.activeUser) ||
-		events.length >= model.data.events[eventId].EventMaxAttendees
-	) {
-		return;
-	}
-	console.log(people);
-	// events.length < eventPartcipantLimit
-	model.data.eventParticipants.push({
-		eventId: eventId,
-		userName: model.app.state.activeUser,
-	});
-	mainView();
+  let people = [];
+  const events = model.data.eventParticipants.filter(
+    (event) => event.eventId === eventId
+  );
+  events.forEach((person) => people.push(person.userName));
+  if (
+    people.includes(model.app.state.activeUser) ||
+    events.length >= model.data.events[eventId].EventMaxAttendees
+  ) {
+    return;
+  }
+  console.log(people);
+  // events.length < eventPartcipantLimit
+  model.data.eventParticipants.push({
+    eventId: eventId,
+    userName: model.app.state.activeUser,
+  });
+  mainView();
 }
 // events.userName === model.app.state.activeUser
 // ||
